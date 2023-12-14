994978f04a141e89541f04b7539aa727cde88f21
NikitaTopchii <nikita.topchii@teamdev.com>
2023-12-14 20:51:02 +0200

    feat(add-new-admin): add permission toggle buttons

--- webapp/admin-bot-webapp/src/app/components/add-admin-page/add-admin-page.component.ts
+++ webapp/admin-bot-webapp/src/app/components/add-admin-page/add-admin-page.component.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
-import {FormBuilder, FormGroup, ReactiveFormsModule, Validators} from "@angular/forms";
+import {FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators} from "@angular/forms";
+import {_MatSlideToggleRequiredValidatorModule, MatSlideToggleModule} from "@angular/material/slide-toggle";
+import {MatButtonModule} from "@angular/material/button";
 
 @Component({
   selector: 'app-add-admin-page',
   standalone: true,
   imports: [
-    ReactiveFormsModule
+    MatSlideToggleModule,
+    FormsModule,
+    _MatSlideToggleRequiredValidatorModule,
+    MatButtonModule,
+    ReactiveFormsModule,
   ],
   templateUrl: './add-admin-page.component.html',
   styleUrl: './add-admin-page.component.scss'
@@ -13,10 +19,19 @@ import {FormBuilder, FormGroup, ReactiveFormsModule, Validators} from "@angular/
 export class AddAdminPageComponent {
 
   public form: FormGroup;
+  public permissionsForm: FormGroup;
+
+  public selectAdminFromList = false;
+  public actionWithCompetition = false;
+  public actionWithStatistic =  false;
+  public permissionToTokenAndPrices = false;
+  public actionWithChatSecurity = false;
+  public editPermission = false;
 
 
   constructor(private readonly fb: FormBuilder) {
     this.form = this.getSignInForm();
+    this.permissionsForm = this.getPermissionsForm();
   }
 
   public signUp(form: FormGroup): void {
@@ -24,7 +39,22 @@ export class AddAdminPageComponent {
   }
   private getSignInForm(): FormGroup {
     return this.fb.group({
-      tagname: ["", [Validators.required]],
+      email: ["", [Validators.required]],
     });
+  }
+
+  private getPermissionsForm(){
+    return this.fb.group({
+      selectAdminFromList: [this.selectAdminFromList],
+      actionWithCompetition: [this.actionWithCompetition],
+      actionWithStatistic: [this.actionWithStatistic],
+      permissionToTokenAndPrices: [this.permissionToTokenAndPrices],
+      actionWithChatSecurity: [this.actionWithChatSecurity],
+      editPermission: [this.editPermission]
+    })
+  }
+
+  alertFormValues(formGroup: FormGroup) {
+    alert(JSON.stringify(formGroup.value, null, 2));
   }
 }
