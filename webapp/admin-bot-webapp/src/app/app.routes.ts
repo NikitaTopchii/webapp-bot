3ed090a3da5d827e7a7cd0833758cb7b688ce018
NikitaTopchii <nikita.topchii@teamdev.com>
2023-12-14 17:38:29 +0200

    feat(add-new-admin): create add-new-admin component

--- webapp/admin-bot-webapp/src/app/app.routes.ts
+++ webapp/admin-bot-webapp/src/app/app.routes.ts
@@ -1,9 +1,14 @@
 import { Routes } from '@angular/router';
 import {AdminListComponent} from "./components/admin-list/admin-list.component";
+import {AddAdminPageComponent} from "./components/add-admin-page/add-admin-page.component";
 
 export const routes: Routes = [
   {
     path: '',
     component: AdminListComponent
+  },
+  {
+    path: 'add-new-admin',
+    component: AddAdminPageComponent
   }
 ];
--- webapp/admin-bot-webapp/src/app/components/add-admin-page/add-admin-page.component.ts
+++ webapp/admin-bot-webapp/src/app/components/add-admin-page/add-admin-page.component.ts
@@ -1,12 +1,30 @@
 import { Component } from '@angular/core';
+import {FormBuilder, FormGroup, ReactiveFormsModule, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-add-admin-page',
   standalone: true,
-  imports: [],
+  imports: [
+    ReactiveFormsModule
+  ],
   templateUrl: './add-admin-page.component.html',
   styleUrl: './add-admin-page.component.scss'
 })
 export class AddAdminPageComponent {
 
+  public form: FormGroup;
+
+
+  constructor(private readonly fb: FormBuilder) {
+    this.form = this.getSignInForm();
+  }
+
+  public signUp(form: FormGroup): void {
+    console.log(form.value);
+  }
+  private getSignInForm(): FormGroup {
+    return this.fb.group({
+      tagname: ["", [Validators.required]],
+    });
+  }
 }
