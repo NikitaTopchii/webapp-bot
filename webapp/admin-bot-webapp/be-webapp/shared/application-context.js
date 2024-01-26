f8c957e40287eafeba4e9245201c5ede82e75a14
Denys Sukhomlyn <denissuhomlin@Deniss-MacBook-Pro.local>
2024-01-26 02:05:54 +0200

    Stop tracking ignored files

--- webapp/admin-bot-webapp/be-webapp/shared/application-context.js
+++
@@ -1,2 +0,0 @@
-module.exports.main_url = 'https://4edd-46-98-213-149.ngrok-free.app';
-module.exports.user_id = '591498550'
--- webapp/admin-bot-webapp/src/app/components/shared/application-context.ts
+++
@@ -1,4 +0,0 @@
-
-//export const main_url = 'https://prizebot.online';
-export const main_url = 'https://6f8f-87-244-173-37.ngrok-free.app'
-
