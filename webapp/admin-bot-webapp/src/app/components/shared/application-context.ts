3d33c42fd2897bf35930d05237bc6eb9289fd5ef
NikitaTopchii <nikita.topchii@teamdev.com>
2024-02-14 15:12:06 +0200

    feat(setup-webhooks): fix getting delayed competitions

--- webapp/admin-bot-webapp/src/app/components/shared/application-context.ts
+++ webapp/admin-bot-webapp/src/app/components/shared/application-context.ts
@@ -3,4 +3,4 @@
 export const main_url = 'https://5f2d-87-244-172-253.ngrok-free.app';
 export const user_id = '591498550'
 //
-// export const bot_webhook_url = 'http:/contest-bot-webhook-url-1845.eu-central-1.elasticbeanstalk.com'
+//export const bot_webhook_url = 'http:/contest-bot-webhook-url-1845.eu-central-1.elasticbeanstalk.com'
