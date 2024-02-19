fa84d33023813a6c2d401b591c1a4675bbcd9856
NikitaTopchii <nikita.topchii@teamdev.com>
2024-02-19 20:44:18 +0200

    feature(admin-store-endpoints): create database for admins store creator

---
+++ webapp/admin-bot-webapp/be-webapp/shared/logger/logger.js
@@ -0,0 +1,16 @@
+
+let colors = require('colors')
+const logger = require('tracer').colorConsole({
+  filters: [
+    colors.underline,
+    colors.white,
+    {
+      trace: colors.bgCyan,
+      info: colors.green,
+      warn: colors.yellow,
+      error: [colors.red, colors.bold]
+    }
+  ]
+})
+
+module.exports.logger = logger;
