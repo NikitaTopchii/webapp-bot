11f6fcd0dbe78d9bbe5b4935a2214b66f5b39dc2
NikitaTopchii <nikita.topchii@teamdev.com>
2024-02-22 02:37:07 +0200

    feature(admin-store-endpoints): fix create store

--- webapp/admin-bot-webapp/src/app/components/shared/application-context.ts
+++ webapp/admin-bot-webapp/src/app/components/shared/application-context.ts
@@ -1,6 +1,6 @@
 
 //export const main_url = 'https://prizebot.online';
-export const main_url = 'https://2098-87-244-172-253.ngrok-free.app';
-export const user_id = '566284527'
+export const main_url = 'https://726c-46-118-104-44.ngrok-free.app';
+// export const user_id = '566284527'
 //
 //export const bot_webhook_url = 'http:/contest-bot-webhook-url-1845.eu-central-1.elasticbeanstalk.com'
