e932b9e3bb2386f4c3b5a0d33e97b39e3c92d923
Denys Sukhomlyn <denissuhomlin@Deniss-MBP.netis.cc>
2024-02-23 16:55:25 +0200

    Implement store and product management features in admin bot webapp
    This update adds new functionalities to the `StoreService` for better store and product management. It introduces methods for creating stores and products, retrieving a list of stores owned by a user, fetching store details by store ID, and product list per store. The `store-main.component.ts`, `store-list.component.ts`, and `store-details.component.ts` are also updated to incorporate these new service methods and improve the user interface by displaying store details and associated product lists. Moreover, the URL configuration in `application-context.ts` has been updated.

--- webapp/admin-bot-webapp/src/app/components/store/components/store-details/store-details.component.ts
+++ webapp/admin-bot-webapp/src/app/components/store/components/store-details/store-details.component.ts
@@ -1,12 +1,24 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from "@angular/router";
+import { StoreService } from "../../service/store.service";
+import { Observable, switchMap } from "rxjs";
+import { ProductListComponent } from "../product-list/product-list.component";
 
 @Component({
   selector: 'app-store-details',
   standalone: true,
-  imports: [],
+  imports: [
+    ProductListComponent
+  ],
   templateUrl: './store-details.component.html',
   styleUrl: './store-details.component.scss'
 })
 export class StoreDetailsComponent {
-
+  public currentStore$ = this.route.params.pipe(
+    switchMap(params => this.storeService.getStoreById(params['id']))
+  )
+    constructor(private route: ActivatedRoute,
+                private storeService: StoreService) {
+    this.currentStore$.subscribe(console.log)
+    }
 }
--- webapp/admin-bot-webapp/src/app/components/store/components/store-list/store-list.component.ts
+++ webapp/admin-bot-webapp/src/app/components/store/components/store-list/store-list.component.ts
@@ -1,12 +1,25 @@
 import { Component } from '@angular/core';
+import { StoreService } from "../../service/store.service";
+import { AsyncPipe } from "@angular/common";
+import { ActivatedRoute, Router } from "@angular/router";
 
 @Component({
   selector: 'app-store-list',
   standalone: true,
-  imports: [],
+  imports: [
+    AsyncPipe
+  ],
   templateUrl: './store-list.component.html',
   styleUrl: './store-list.component.scss'
 })
 export class StoreListComponent {
+  private readonly owner_id = localStorage.getItem('user_id') || '';
+  constructor(private storeService: StoreService,
+              private router: Router,
+              private route: ActivatedRoute) { }
+  public stores$ = this.storeService.getStores$(this.owner_id);
 
+  public redirectToStoreDetails(store_id: string): void {
+    this.router.navigate([store_id], {relativeTo: this.route});
+  }
 }
